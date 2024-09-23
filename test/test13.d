4d2d8366cd6248aed257be69df40729d985b8f0a
friji <67202651+friji350@users.noreply.github.com>
2024-09-23 21:19:21 +0300

    te

---
+++ test/test13.d
@@ -0,0 +1,7 @@
+var x := 5;
+if x is real or x - 3 < 20 then
+    var a := 3 + (2 * 3);
+    var b := 3;
+else
+    var a := 4;
+end;
---
+++ test/test9.d
@@ -0,0 +1,7 @@
+var x := 5;
+if not x + 3 > 10 and x < 20 then
+    var a := 3 + (2 * 3);
+    var b := 3;
+else
+    var a := 4;
+end;
